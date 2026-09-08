8bc2937cc01e78c53a51d5358239a3caefd19735
Pragyvis <pragyavishwakarma2000@gmail.com>
2026-09-08 11:46:11 +0530

    1. Added files to Github

---
+++ SamplePython.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! CI/CD deployment is working successfully 🚀"
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "UP",
+        "message": "Application is healthy"
+    }), 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
